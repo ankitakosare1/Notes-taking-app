71f87141c7e32a9abd366af4927888e4fb064d5f
= <=>
2025-07-04 23:54:55 +0530

    Fix: Added alt attribute to image

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ import { FaLock } from 'react-icons/fa';
 const LandingPage = () => {
   return (
     <div className='landingPage'>
-      <img src={backgroundImage} className='backgroundImage' />
+      <img src={backgroundImage} className='backgroundImage' alt='Notebook-Image'/>
       <h1>Pocket Notes</h1>
       <p>Send and receive messages without keeping your phone online.</p>
       <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
